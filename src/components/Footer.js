8ace1b207d049fa5a4e7a3c4e8fb5a80fd3cc214
sehouse <sehouse@umich.edu>
2020-08-16 14:58:45 -0400

    Footer written.

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from "react";
+
+const Footer = () => {
+  return (
+    <footer className="site-footer site-footer--with-clipmask">
+      <div className="site-footer__quote">
+        "Fear nothing and work hard. One thing you'll discover is that life is
+        based less than you think on what you've learned, and much more than you
+        think on what you have inside you right from the beginning.”"
+      </div>
+      <div className="site-social">
+        <div className="site-social__list">
+          <a href="https://github.com/sehouse" className="site-social__link">
+            <svg
+              viewBox="0 0 512 512"
+              preserveAspectRatio="xMidYMid meet"
+              className="site-social__icon"
+            >
+              <path d="M256 70.7c-102.6 0-185.9 83.2-185.9 185.9 0 82.1 53.3 151.8 127.1 176.4 9.3 1.7 12.3-4 12.3-8.9V389.4c-51.7 11.3-62.5-21.9-62.5-21.9 -8.4-21.5-20.6-27.2-20.6-27.2 -16.9-11.5 1.3-11.3 1.3-11.3 18.7 1.3 28.5 19.2 28.5 19.2 16.6 28.4 43.5 20.2 54.1 15.4 1.7-12 6.5-20.2 11.8-24.9 -41.3-4.7-84.7-20.6-84.7-91.9 0-20.3 7.3-36.9 19.2-49.9 -1.9-4.7-8.3-23.6 1.8-49.2 0 0 15.6-5 51.1 19.1 14.8-4.1 30.7-6.2 46.5-6.3 15.8 0.1 31.7 2.1 46.6 6.3 35.5-24 51.1-19.1 51.1-19.1 10.1 25.6 3.8 44.5 1.8 49.2 11.9 13 19.1 29.6 19.1 49.9 0 71.4-43.5 87.1-84.9 91.7 6.7 5.8 12.8 17.1 12.8 34.4 0 24.9 0 44.9 0 51 0 4.9 3 10.7 12.4 8.9 73.8-24.6 127-94.3 127-176.4C441.9 153.9 358.6 70.7 256 70.7z"></path>
+            </svg>
+          </a>
+        </div>
+        <div className="site-social__list">
+          <a
+            href="https://www.linkedin.com/in/sehouse/"
+            className="site-social__link"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              className="site-social__icon"
+            >
+              <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 8c0 .557-.447 1.008-1 1.008s-1-.45-1-1.008c0-.557.447-1.008 1-1.008s1 .452 1 1.008zm0 2h-2v6h2v-6zm3 0h-2v6h2v-2.861c0-1.722 2.002-1.881 2.002 0v2.861h1.998v-3.359c0-3.284-3.128-3.164-4-1.548v-1.093z" />
+            </svg>
+          </a>
+        </div>
+        <div className="site-social__list">
+          <a
+            href="https://drive.google.com/file/d/1CUnewHMnXm_YopK0P_lNEaPJ7eXIXMBM/view?usp=sharing"
+            className="site-social__link"
+          >
+            <svg
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              viewBox="0 0 24 24"
+              className="site-social__icon"
+            >
+              <path d="M9.484 15.696l-.711-.696-2.552 2.607-1.539-1.452-.698.709 2.25 2.136 3.25-3.304zm0-5l-.711-.696-2.552 2.607-1.539-1.452-.698.709 2.25 2.136 3.25-3.304zm0-5l-.711-.696-2.552 2.607-1.539-1.452-.698.709 2.25 2.136 3.25-3.304zm10.516 11.304h-8v1h8v-1zm0-5h-8v1h8v-1zm0-5h-8v1h8v-1zm4-5h-24v20h24v-20zm-1 19h-22v-18h22v18z" />
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div className="site-footer__copyright"> Copyright Scott House 2020 </div>
+    </footer>
+  );
+};
+
+export default Footer;
