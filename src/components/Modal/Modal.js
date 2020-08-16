0670eb31b694b971663c2fecf0dd8c29a3109a63
sehouse <sehouse@umich.edu>
2020-08-16 12:28:24 -0400

    Modal is written.

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import "./Modal.css";
+
+const Modal = (props) => (
+    <div className="modal">
+        <div className="modal-content">
+            <div className="modal-body">
+                {props.children}
+            </div>
+        </div>
+    </div>
+)
+
+export default Modal;
